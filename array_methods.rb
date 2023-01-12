99e8c71f4eab164c848d013b802b222f565727b9
“AnujPAtel77” <anuj@softcodingsolutions.com>
2023-01-12 17:59:52 +0530

    array methods started

--- array_methods.rb
+++ array_methods.rb
@@ -9,3 +9,11 @@ puts "array - #{ary1}"
 
 arr = [1, 2, 3, 4, 5, 6]
 puts "array - #{arr[1..-3]}"
+puts "#{arr.at(0)}"
+puts "#{arr.fetch(1)}"
+puts "#{arr.first}"
+puts "#{arr.last}"
+# first n elements of an array
+puts "#{arr.take(3)}"
+# last n elements of an array
+puts "#{arr.drop(3)}"
